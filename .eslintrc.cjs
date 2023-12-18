09970c9d8a596dca8585b871a5c6ea2e09049555
Victoria Kapitonenko <kapitonenko.vick@ya.ru>
2023-12-18 18:07:33 +0300

    chore: disable rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,4 +4,8 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     root: true,
+    rules: {
+      "no-unused-vars": 0,
+      "@typescript-eslint/no-unused-vars": 0
+    }
   };
